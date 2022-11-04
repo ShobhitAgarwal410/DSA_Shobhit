8ec0708624040bc8326994fe281f23d131d48843
shoagarw2 <shoagarw2@publicisgroupe.net>
2022-11-05 01:39:08 +0530

    Array Day2

---
+++ src/study/interviewbit/Array/MaxMinSum.java
@@ -0,0 +1,36 @@
+package study.interviewbit.Array;
+
+import java.util.ArrayList;
+
+public class MaxMinSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> A = new ArrayList<Integer>();
+		A.add(22);
+		A.add(6);
+		A.add(4);
+		A.add(3);
+		A.add(6);
+		A.add(6);
+		A.add(3);
+		A.add(2);
+		A.add(2);
+		A.add(-5);
+		A.add(8);
+		System.out.println(solve(A));
+	}
+
+	public static int solve(ArrayList<Integer> A) {
+		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE; 
+		for (int i = 0; i < A.size(); i++) {
+			if(A.get(i)>max) {
+				max = A.get(i);
+			}
+			if(A.get(i)<min) {
+				min = A.get(i);
+			}
+		}
+		return max+min;
+	}
+}
