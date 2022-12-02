df2e24190bb5f485d31422e57b9ec8c62893830a
Shobhit Agarwal <shoagarw2@publicisgroupe.net>
2022-12-02 13:06:53 +0530

    Array ques

---
+++ src/study/interviewbit/StringQues/ExcelColumnNum.java
@@ -0,0 +1,24 @@
+package study.interviewbit.StringQues;
+
+public class ExcelColumnNum {
+
+	public static void main(String[] args) {
+		String A = "BDE";
+		titleToNumber(A);
+	}
+
+	public static int titleToNumber(String A) {
+		int n = A.length();
+		int ans = 0;
+		int p = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			int c = A.charAt(i);
+			int v = c - 64;
+			ans += (int) Math.pow(26, p) * v;
+			p++;
+		}
+
+		return ans;
+	}
+
+}
